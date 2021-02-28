6a065b4ba1e687e2af3688680bb67284af2407da
Akuli <akuviljanen17@gmail.com>
2021-02-28 21:49:21 +0200

    combine math.c and numbers.c

--- lib/numbers.c
+++ lib/numbers.c
@@ -51,3 +51,12 @@ double meth_string_to_float(const struct String *s)
 	assert(errno == 0 && endptr == s->str + strlen(s->str));  // TODO: exceptions or optionals
 	return res;
 }
+
+int64_t int_mod(int64_t a, int64_t b)
+{
+	int64_t res = a % b;
+	// Make sure result has same sign as b
+	if ((b > 0 && res < 0) || (b < 0 && res > 0))
+		res += b;
+	return res;
+}
