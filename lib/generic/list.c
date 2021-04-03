Akuli/oomph

commit ebee6bfe23b843abb1148bdea3eb97fb71a69c6f

return deleted item in delete_at_index (#127)
* return deleted item in delete_at_index

* use in self-hosted

lib/generic/list.c
@@ -108,12 +108,13 @@ ITEMTYPE METHOD(get)(TYPE self, int64_t i)
 	return self->data[i];
 }
 
-void METHOD(delete_at_index)(TYPE self, int64_t i)
+ITEMTYPE METHOD(delete_at_index)(TYPE self, int64_t i)
 {
 	validate_index(self, i);
-	DECREF_ITEM(self->data[i]);
+	ITEMTYPE item = self->data[i];
 	self->len--;
 	memmove(self->data+i, self->data+i+1, (self->len - i)*sizeof(self->data[0]));
+	return item;
 }
 
 static TYPE slice(TYPE self, int64_t start, int64_t end, bool del)

pyoomph/types.py
@@ -118,7 +118,7 @@ class Generic:
         result.constructor_argtypes = []
         # TODO: hide __contains better?
         result.methods["__contains"] = FunctionType([result, generic_arg], BOOL)
-        result.methods["delete_at_index"] = FunctionType([result, INT], None)
+        result.methods["delete_at_index"] = FunctionType([result, INT], generic_arg)
         result.methods["delete_slice"] = FunctionType([result, INT, INT], result)
         result.methods["ends_with"] = FunctionType([result, result], BOOL)
         result.methods["find_first"] = FunctionType([result, generic_arg], INT)

self_hosted/ast2ir.oomph
@@ -967,8 +967,7 @@ class FileConverter(
         # TODO: better list methods
         let laziness_names = [foreach pair of self.typedef_laziness: pair.name]
         if name in laziness_names:
-            let pair = self.typedef_laziness.get(laziness_names.find_only(name))
-            self.typedef_laziness.delete_at_index(laziness_names.find_only(name))
+            let pair = self.typedef_laziness.delete_at_index(laziness_names.find_only(name))
             assert(name not in [foreach t of self.types: t.name])
             self.types.push(new NamedType(name, self.get_type(pair.type, null)))
 

self_hosted/ir.oomph
@@ -174,7 +174,7 @@ export class Builtins(
         methods.push_all([
             # TODO: hide __contains better?
             new Method("__contains", new FunctionType([r, arg], self.BOOL)),
-            new Method("delete_at_index", new FunctionType([r, self.INT], null)),
+            new Method("delete_at_index", new FunctionType([r, self.INT], arg)),
             new Method("delete_slice", new FunctionType([r, self.INT, self.INT], r)),
             new Method("ends_with", new FunctionType([r, r], self.BOOL)),
             new Method("find_first", new FunctionType([r, arg], self.INT)),

self_hosted/main.oomph
@@ -63,22 +63,20 @@ export func main():
     let valgrind = ""
 
     while args != [] and args.first().starts_with("--"):
-        if args.first() == "--tokens":
+        let arg = args.delete_at_index(0)
+        if arg == "--tokens":
             tokens_only = true
-        elif args.first() == "--ast":
+        elif arg == "--ast":
             ast_only = true
-        elif args.first().starts_with("--valgrind="):
-            valgrind = args.first().remove_prefix("--valgrind=")
+        elif arg.starts_with("--valgrind="):
+            valgrind = arg.remove_prefix("--valgrind=")
         else:
-            print("bad arg: {args.first()}")
+            print("bad arg: {arg}")
             assert(false)  # TODO: error
-
-        args.delete_at_index(0)
 
     assert(not (ast_only and tokens_only))
     assert(args != [])  # TODO: error
-    let source_path = args.first()
-    args.delete_at_index(0)
+    let source_path = args.delete_at_index(0)
 
     let builtins = ir::create_builtins()
     let code = io::read_file(source_path)

tests/list_delete.oomph
@@ -1,6 +1,6 @@
 export func main():
     let foo = ["a", "b", "c"]
-    foo.delete_at_index(0)
+    print("Deleted: " + foo.delete_at_index(0))
     print(foo)
-    foo.delete_at_index(1)
+    print("Deleted: " + foo.delete_at_index(1))
     print(foo)

commit fd15735dbe80eaa865ff11b13903e2edacffa73a

fix refcount bug introduced in #127

lib/generic/list.c
@@ -209,17 +209,20 @@ int64_t METHOD(find_only)(TYPE self, ITEMTYPE item)
 
 void METHOD(delete_first)(TYPE self, ITEMTYPE item)
 {
-	METHOD(delete_at_index)(self, METHOD(find_first)(self, item));
+	ITEMTYPE deleted = METHOD(delete_at_index)(self, METHOD(find_first)(self, item));
+	DECREF_ITEM(deleted);
 }
 
 void METHOD(delete_last)(TYPE self, ITEMTYPE item)
 {
-	METHOD(delete_at_index)(self, METHOD(find_last)(self, item));
+	ITEMTYPE deleted = METHOD(delete_at_index)(self, METHOD(find_last)(self, item));
+	DECREF_ITEM(deleted);
 }
 
 void METHOD(delete_only)(TYPE self, ITEMTYPE item)
 {
-	METHOD(delete_at_index)(self, METHOD(find_only)(self, item));
+	ITEMTYPE deleted = METHOD(delete_at_index)(self, METHOD(find_only)(self, item));
+	DECREF_ITEM(deleted);
 }
 
 bool METHOD(starts_with)(TYPE self, TYPE prefix)