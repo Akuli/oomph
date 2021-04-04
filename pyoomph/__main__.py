7343dcdd7e9c4cd8e1d1f33083d5b9bcda846d97
PurpleMyst <PurpleMyst@users.noreply.github.com>
2021-04-04 19:11:32 +0200

    refactor: refactor __main__.py to use functions

--- pyoomph/__main__.py
+++ pyoomph/__main__.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import argparse
 import atexit
 import itertools
-import pathlib
+from pathlib import Path
 import shlex
 import shutil
 import signal
@@ -14,14 +14,14 @@ from typing import Dict, List, Optional, Tuple
 
 from pyoomph import ast, ast2ir, ast_transformer, c_output, ir, parser
 
-python_code_dir = pathlib.Path(__file__).absolute().parent
+python_code_dir = Path(__file__).absolute().parent
 project_root = python_code_dir.parent
 
 
 class CompilationUnit:
     ast: List[ast.ToplevelDeclaration]
 
-    def __init__(self, source_path: pathlib.Path, session: c_output.Session):
+    def __init__(self, source_path: Path, session: c_output.Session):
         self.source_path = source_path
         self.session = session
 
@@ -49,9 +49,7 @@ class CompilationUnit:
             self._handle_error()
 
 
-def get_c_compiler_command(
-    c_paths: List[pathlib.Path], exepath: pathlib.Path
-) -> Tuple[List[str], str]:
+def get_c_compiler_command(c_paths: List[Path], exepath: Path) -> Tuple[List[str], str]:
     compile_info = {}
     with (project_root / "obj" / "compile_info.txt").open() as file:
         for line in file:
@@ -86,7 +84,7 @@ def run(command: List[str], verbose: bool, human_readable: Optional[str] = None)
     return subprocess.run(command).returncode
 
 
-def get_compilation_dir(parent_dir: pathlib.Path, name_hint: str) -> pathlib.Path:
+def get_compilation_dir(parent_dir: Path, name_hint: str) -> Path:
     for i in itertools.count():
         path = parent_dir / (name_hint + str(i))
         path.mkdir(parents=True, exist_ok=True)
@@ -101,10 +99,82 @@ def get_compilation_dir(parent_dir: pathlib.Path, name_hint: str) -> pathlib.Pat
     assert False  # make mypy feel good
 
 
+def compute_dependency_graph(
+    session: c_output.Session,
+    infile: Path,
+    verbose: bool,
+) -> Tuple[List[CompilationUnit], Dict[Path, List[Path]]]:
+    compilation_units: List[CompilationUnit] = []
+    dependency_graph: Dict[Path, List[Path]] = {}
+    queue = [infile]
+    while queue:
+        # Pop the next source file to parse
+        source_path = queue.pop()
+        if source_path in dependency_graph:
+            continue
+        if verbose:
+            print("Parsing", source_path)
+
+        # Create a compilation unit out of it and parse it into an untyped ast
+        candidate_unit = CompilationUnit(source_path, session)
+        compilation_units.append(candidate_unit)
+        candidate_unit.create_untyped_ast()
+
+        # Calculate its dependencies and add them to the dependencies dictionary,
+        # including builtins if necessary, and add those dependencies to the queue
+        current_dependencies = [
+            top_declaration.path
+            for top_declaration in candidate_unit.ast
+            if isinstance(top_declaration, ast.Import)
+        ]
+        if source_path != project_root / "builtins.oomph":
+            current_dependencies.append(project_root / "builtins.oomph")
+        dependency_graph[candidate_unit.source_path] = current_dependencies
+        queue.extend(current_dependencies)
+    return compilation_units, dependency_graph
+
+
+def compute_compilation_order(
+    verbose: bool,
+    compilation_units: List[CompilationUnit],
+    dependency_graph: Dict[Path, List[Path]],
+) -> List[CompilationUnit]:
+    compilation_order: List[CompilationUnit] = []
+    while len(compilation_order) < len(dependency_graph):
+        candidate_unit = next(
+            u for u in compilation_units if u not in compilation_order
+        )
+        breadcrumbs = [candidate_unit]
+        while True:
+            uncompiled_dependencies = [
+                u
+                for u in compilation_units
+                if u not in compilation_order
+                and u.source_path in dependency_graph[candidate_unit.source_path]
+            ]
+            if not uncompiled_dependencies:
+                break
+            if verbose:
+                print(
+                    f"Noticed dependency: {candidate_unit.source_path} --> {uncompiled_dependencies[0].source_path}"
+                )
+            candidate_unit = uncompiled_dependencies[0]
+            if candidate_unit in breadcrumbs:
+                message = (
+                    " --> ".join(d.source_path.name for d in breadcrumbs)
+                    + " --> "
+                    + candidate_unit.source_path.name
+                )
+                raise RuntimeError("cyclic imports: " + message)
+            breadcrumbs.append(candidate_unit)
+        compilation_order.append(candidate_unit)
+    return compilation_order
+
+
 def main() -> None:
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("infile", type=pathlib.Path)
-    arg_parser.add_argument("-o", "--outfile", type=pathlib.Path)
+    arg_parser.add_argument("infile", type=Path)
+    arg_parser.add_argument("-o", "--outfile", type=Path)
     arg_parser.add_argument("--valgrind", default="")
     arg_parser.add_argument("-v", "--verbose", action="store_true")
     compiler_args, program_args = arg_parser.parse_known_args()
@@ -113,76 +183,39 @@ def main() -> None:
         cache_dir = compiler_args.infile.parent / ".oomph-cache"
         cache_dir.mkdir(exist_ok=True)
     except OSError:
-        cache_dir = pathlib.Path.cwd() / ".oomph-cache"
+        cache_dir = Path.cwd() / ".oomph-cache"
         cache_dir.mkdir(exist_ok=True)
 
+    # Create a compiler session
     session = c_output.Session(
         get_compilation_dir(cache_dir, compiler_args.infile.stem + "_compilation")
     )
 
-    all_compilation_units: List[CompilationUnit] = []
-    dependencies: Dict[pathlib.Path, List[pathlib.Path]] = {}
-    todo_list = [compiler_args.infile.absolute()]
-    while todo_list:
-        source_path = todo_list.pop()
-        if source_path in dependencies:
-            continue
+    # Calculate the dependency graph
+    compilation_units, dependency_graph = compute_dependency_graph(
+        session, compiler_args.infile.absolute(), compiler_args.verbose
+    )
 
-        if compiler_args.verbose:
-            print("Parsing", source_path)
+    # Calculate in which order we need to compile our units
+    compilation_order = compute_compilation_order(
+        compiler_args.verbose, compilation_units, dependency_graph
+    )
 
-        unit = CompilationUnit(source_path, session)
-        all_compilation_units.append(unit)
-        unit.create_untyped_ast()
-
-        deps = [
-            top_declaration.path
-            for top_declaration in unit.ast
-            if isinstance(top_declaration, ast.Import)
-        ]
-        if source_path != project_root / "builtins.oomph":
-            deps.append(project_root / "builtins.oomph")
-        dependencies[unit.source_path] = deps
-        todo_list.extend(deps)
-
-    # Dumbest toposort you have ever seen
-    compilation_order: List[CompilationUnit] = []
-    while len(compilation_order) < len(dependencies):
-        unit = [u for u in all_compilation_units if u not in compilation_order][0]
-        decisions = [unit]
-        while True:
-            need_first = [
-                u
-                for u in all_compilation_units
-                if u not in compilation_order
-                and u.source_path in dependencies[unit.source_path]
-            ]
-            if not need_first:
-                break
-            if compiler_args.verbose:
-                print(
-                    f"Noticed dependency: {unit.source_path} --> {need_first[0].source_path}"
-                )
-            unit = need_first[0]
-            decisions.append(unit)
-            if decisions.count(unit) >= 2:
-                message = " --> ".join(d.source_path.name for d in decisions)
-                raise RuntimeError("cyclic imports: " + message)
-        compilation_order.append(unit)
-
+    # Compile in the calculated order
     for unit in compilation_order:
         if compiler_args.verbose:
             print("Creating C code:", unit.source_path)
         unit.create_c_code(session.symbols)
 
+    # Write out everything and compile it
     c_paths = session.write_everything(project_root / "builtins.oomph")
     exe_path = session.compilation_dir / compiler_args.infile.stem
     command, human_readable_command = get_c_compiler_command(c_paths, exe_path)
-
     result = run(command, compiler_args.verbose, human_readable_command)
     if result != 0:
         sys.exit(result)
 
+    # If we have an outfile path, move the resulting executable to it and bail
     if compiler_args.outfile is not None:
         assert not compiler_args.outfile.is_dir()  # shutil.move is weird for dirs
         shutil.move(str(exe_path), str(compiler_args.outfile))
@@ -190,8 +223,8 @@ def main() -> None:
             print("Moved executable to", compiler_args.outfile)
         return
 
+    # Otherwise, run it directly
     command = shlex.split(compiler_args.valgrind) + [str(exe_path)] + program_args
-
     result = run(command, compiler_args.verbose)
     if result < 0:  # killed by signal
         message = f"Program killed by signal {abs(result)}"
