04e3535faf96e98e48ad822064c76b81a641bbf0
Akuli <akuviljanen17@gmail.com>
2021-04-11 15:29:56 +0300

    list copy method

--- pyoomph/types.py
+++ pyoomph/types.py
@@ -139,6 +139,7 @@ class Generic:
             result.constructor_argtypes = []
             # TODO: hide __contains better?
             result.methods["__contains"] = FunctionType([result, itemtype], BOOL)
+            result.methods["copy"] = FunctionType([result], result)
             result.methods["delete_at_index"] = FunctionType([result, INT], itemtype)
             result.methods["delete_slice"] = FunctionType([result, INT, INT], result)
             result.methods["ends_with"] = FunctionType([result, result], BOOL)
