4505f9244a8652e626f7b8584478ee5b695f7345
Akuli <akuviljanen17@gmail.com>
2021-04-26 15:49:18 +0300

    lint

--- pyoomph/ast2ir.py
+++ pyoomph/ast2ir.py
@@ -872,7 +872,11 @@ class _FileConverter:
             raise RuntimeError("can't use auto type here")
         elif isinstance(raw_type, ast.FunctionType):
             args = [recursing_callback(arg) for arg in raw_type.argtypes]
-            returntype = None if raw_type.returntype is None else recursing_callback(raw_type.returntype)
+            returntype = (
+                None
+                if raw_type.returntype is None
+                else recursing_callback(raw_type.returntype)
+            )
             return ir.FunctionType(args, returntype)
         elif isinstance(raw_type, ast.NamedType):
             return self.get_named_type(raw_type.name)
